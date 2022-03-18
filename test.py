d1f81f9d6d774a165caaeeacd636d5b84f7c2ead
CeritaMustofa <mustofa@mustofa.pinuin>
2022-03-18 01:53:39 -0700

    first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+from math import radians
+
+
+panjang_range = 6
+
+frontend = [3,]
+backend = [5,]
+
+for i in range(1, panjang_range):
+    if i == frontend:
+        panjang_range.replace(i, 'frontend')
+    
+    
+    print(i)
